0d88d5c2eb36d6106e94ff6150a55ebee44b7ea6
zabelin <zabelin_maxim@bk.ru>
2023-02-16 19:15:58 +0700

    added a class that invoking a sorting and system writer for prepared array

---
+++ src/main/java/ru/testproject/readers/SortingMultipartFiles.java
@@ -0,0 +1,56 @@
+package ru.testproject.readers;
+
+import org.springframework.stereotype.Service;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Objects;
+
+@Service
+public class SortingMultipartFiles {
+
+    private final ParsingArgs parsingArgs;
+    private final PreparingArrays preparingArrays;
+    private final Sorting sorting;
+
+
+    public SortingMultipartFiles(ParsingArgs parsingArgs, PreparingArrays preparingArrays, Sorting sorting) {
+        this.parsingArgs = parsingArgs;
+        this.preparingArrays = preparingArrays;
+        this.sorting = sorting;
+    }
+
+    public void sortingIntegerFiles() throws Exception {
+        List<?> input = preparingArrays.checkingArray();
+        int size = input.size();
+        int[] array = new int[size];
+
+        for (int i = 0; i < size; i++) {
+            array[i] = (int) input.get(i);
+        }
+
+        Sorting.mergeSortInteger(array, size, preparingArrays.getParsingArgs()[0]);
+
+        List<String> result = Arrays.stream(array).mapToObj(Objects::toString).toList();
+        FileSystemWriter outputFile = new FileSystemWriter(preparingArrays.getParsingArgs()[2]);
+        outputFile.setLines(result);
+
+    }
+
+    public void sortingStringFiles() throws Exception {
+
+        List<?> input = preparingArrays.checkingArray();
+        int size = input.size();
+        String[] array = new String[size];
+        for (int i = 0; i < size; i++) {
+            array[i] = (String) input.get(i);
+        }
+        Sorting.mergeSortString(array, size, preparingArrays.getParsingArgs()[0]);
+
+        FileSystemWriter outputFile = new FileSystemWriter(preparingArrays.getParsingArgs()[2]);
+        outputFile.setLines(Arrays.asList(array));
+
+    }
+
+
+}
