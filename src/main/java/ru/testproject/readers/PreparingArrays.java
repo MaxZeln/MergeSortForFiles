72b6db2be4da8c52a52fd045e72a54adc09fd4f7
zabelin <zabelin_maxim@bk.ru>
2023-02-16 19:06:25 +0700

    developed a class which preparing arrays in one array from multiple files, and checking data from them for validity

---
+++ src/main/java/ru/testproject/readers/PreparingArrays.java
@@ -0,0 +1,69 @@
+package ru.testproject.readers;
+
+import org.springframework.stereotype.Service;
+import ru.testproject.readers.exception.ErrorRowCatcher;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class PreparingArrays {
+    private final ParsingArgs parsingArgs;
+
+    public PreparingArrays(ParsingArgs parsingArgs) {
+        this.parsingArgs = parsingArgs;
+    }
+
+    public String[] getParsingArgs() {
+        return parsingArgs.getArgs();
+    }
+
+    public List<String> getArrayFromFile(FileSystemReader file) {
+        try {
+            List<String> lines = file.getLines();
+            if (lines == null) {
+                throw new ErrorRowCatcher("There is no data in file " + file.getFilename());
+            }
+            return lines;
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            return new ArrayList<>();
+        }
+    }
+
+    public List<?> checkingArray() { //type sorting -i -s
+        String[] args = getParsingArgs();
+        switch (args[1]) {
+            case ("-i"): {
+                List<Integer> result = new ArrayList<>();
+                for (int i = 3; i < args.length; i++) {
+                    List<String> arrayFromFile = getArrayFromFile(new FileSystemReader(args[i]));
+                    try {
+                        for (int j = 0; j < arrayFromFile.size(); j++) {
+                            result.add(Integer.parseInt(arrayFromFile.get(j)));
+                        }
+                    } catch (NumberFormatException e) {
+                        throw new NumberFormatException("File with data contains non Integer values");
+                    }
+                }
+                return result;
+            }
+            case ("-s"): {
+                List<String> result = new ArrayList<>();
+                for (int i = 3; i < args.length; i++) {
+                    List<String> arrayFromFile = getArrayFromFile(new FileSystemReader(args[i]));
+                    for (int j = 0; j < arrayFromFile.size(); j++) {
+                        String[] split = arrayFromFile.get(j).split("");
+                        for (String ch : split) {
+                            result.add(ch);
+                        }
+                    }
+                }
+                return result;
+            }
+        }
+        return null;
+    }
+}
+
+
