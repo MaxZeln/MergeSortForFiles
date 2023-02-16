ea7a4241dda3b9f9fce4a95ee001495bb74f1fa1
zabelin <zabelin_maxim@bk.ru>
2023-02-16 18:47:59 +0700

    adding a missed Exception "ErrorRowCatcher"

---
+++ src/main/java/ru/testproject/readers/exception/ErrorRowCatcher.java
@@ -0,0 +1,18 @@
+package ru.testproject.readers.exception;
+
+public class ErrorRowCatcher extends RuntimeException {
+    private String message;
+
+    public ErrorRowCatcher(String error) {
+        super(error);
+        this.message = error;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
