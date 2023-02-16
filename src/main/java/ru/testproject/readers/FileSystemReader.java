7958fe2b3d54d2e1c2be074241ed7d4bad2027b2
zabelin <zabelin_maxim@bk.ru>
2023-02-16 18:45:16 +0700

    developed FileSystemReader and FileSystemWriter for files

--- src/main/java/ru/testproject/readers/FileSystemReader.java
+++ src/main/java/ru/testproject/readers/FileSystemReader.java
@@ -1,36 +1,38 @@
 package ru.testproject.readers;
 
+import ru.testproject.readers.exception.ErrorRowCatcher;
 
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.List;
 
-//получение файла из FileSystem
-public class FileSystemReader implements DataSupplier {
+public class FileSystemReader {
     private String filePath;
 
     public FileSystemReader(String filePath) {
         this.filePath = filePath;
     }
 
-    @Override
-    public List<String> getLines() {
-        Path pathFile = Paths.get(filePath);
+    public List<String> getLines() throws Exception {
+        Path pathFile = Paths.get("src//main//resources//" + filePath);
         boolean exists = Files.exists(pathFile);
-        if (exists == true) {
+        if (exists) {
             try {
                 return Files.readAllLines(pathFile);
             } catch (Exception e) {
-                System.out.println(e.getMessage());
                 System.out.println("There is problem with file " + pathFile.getFileName());
             }
         }
-        return null;
+        throw new ErrorRowCatcher("File is not exist" + pathFile.getFileName());
     }
 
-    @Override
     public String getFilename() {
         return Paths.get(filePath).getFileName().toString();
+    }
+
+    public ArrayList<String> makeArray(List<String> input) {
+        return null;
     }
 }
---
+++ src/main/java/ru/testproject/readers/FileSystemWriter.java
@@ -0,0 +1,30 @@
+package ru.testproject.readers;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+
+public class FileSystemWriter {
+    private String filePath;
+
+    public FileSystemWriter(String filePath) {
+        this.filePath = filePath;
+    }
+
+    public void setLines(List<String> input) {
+        Path pathFile = Paths.get("src//main//resources//" + filePath);
+
+        try {
+            Files.deleteIfExists(pathFile);
+            Files.createFile(pathFile);
+            Files.write(pathFile, input);
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+}
